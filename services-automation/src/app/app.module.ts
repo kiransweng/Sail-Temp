6f1657e9fed8d6f94fa50ad1b67b60b5cb35b484
Vithal R Bonker <vbonker@asiapac.globalcsc.net>
2017-11-09 20:19:06 +0530

    Added angular 2 project with global styling and sample login component

--- services-automation/src/app/app.module.ts
+++ services-automation/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
