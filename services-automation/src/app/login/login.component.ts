a1d6f9f267ab0cb496a81e4631459de185d6be18
Vithal Bonker <vithalbonker@gmail.com>
2017-11-12 23:53:43 +0530

    scripting form controls

--- services-automation/src/app/login/login.component.ts
+++ services-automation/src/app/login/login.component.ts
@@ -8,6 +8,32 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  steps = [
+    {name:'blank', value:'           '},
+    {name:'verifyStatusCode', value:'Verify Status Code'},
+    {name:'getMethod', value:'GET Method'},
+    {name:'postMethod', value:'POST Method'},
+    {name:'patchMethod', value:'PATCH Method'},
+    {name:'deleteMethod', value:'DELETE Method'}
+  ];
+
+  selectedStepValue: string;
+
+  isSelectedStepMatches(stepName):boolean{
+    let nameObj = this.steps.find(res => res.value === this.selectedStepValue);
+    if (nameObj.name === stepName) {
+        this.selectedStepValue = ''
+        return true;
+    }
+    return false;
+  }
+
+
+
+
+
+
+
   login ={
     username:"admin",
     password:"admin"
